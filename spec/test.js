3398bf4bc49fe5292c2869a7b67f5c49ed9e8b5f
shushanfx <shushanfx@gmail.com>
2017-08-04 12:09:01 +0800

    Add ci support.

--- spec/test.js
+++ spec/test.js
@@ -1,19 +1,13 @@
 var FileUtils = require("../src/util/FileUtils")
-var config = require("../src/config");
+var path = require("path");
 
+describe("Test FileUtils", function(){
+	beforeAll(function(){
+		FileUtils.init("/shushanfx/nfile/data");
+	});
 
-describe("I am a test", function() {
-    beforeAll(function() {
-        var SystemConfig = config.getConfig();
-        FileUtils.init(SystemConfig["workspace"]["path"]);
-    });
-
-    it("Test with out filter", function() {
-        console.dir(FileUtils.listFileSync("."));
-    })
-    it("Test with filter", function() {
-        console.dir(FileUtils.listFileSync(".", function(obj) {
-            return obj.ext === "md";
-        }))
-    })
+	it("Get path.", function(){
+		var expectPath = ["", "shushanfx", "nfile", "data", "test", "path"].join(path.sep);
+		expect(FileUtils.getPath("test", "path")).toBe(expectPath);
+	})
 })
